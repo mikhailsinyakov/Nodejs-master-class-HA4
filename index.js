b9d1b5a05617f0137fac24d9f849d3f4f406c558
mikhailsinyakov <mikhail.sinyakov86@gmail.com>
2018-09-12 22:12:52 +0300

    Added cli.js file, added support for basic commands (man, help, stats, exit)

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-
 const server = require('./lib/server');
+const cli = require('./lib/cli');
 
 const app = {};
 
 app.init = () => {
     server.init();
+    setTimeout(() => cli.init(), 50);
 };
 
 app.init();
